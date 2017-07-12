216949ac4141a2760f972b0752d16d8cc0a0b9df
Hinnerk van Bruinehsen <h.v.bruinehsen@fu-berlin.de>
2017-07-12 16:06:48 +0200

    am335x: experimental/WIP support for am335x base-hw

---
+++ repos/base/mk/spec/wega_am335x.mk
@@ -0,0 +1,16 @@
+#
+# \brief  Build-system configurations for Phytec Wega AM335x
+# \author Hinnerk van Bruinehsen
+# \date   2017-03-27
+#
+
+# denote wich specs are also fullfilled by this spec
+SPECS += arm_v6 cortex_a8
+#usb framebuffer gpio
+
+# add repository relative include paths
+REP_INC_DIR += include/spec/wega_am335x
+
+# include implied specs
+include $(call select_from_repositories,mk/spec/cortex_a8.mk)
+#include $(call select_from_repositories,mk/spec/pl011.mk)
