34a7295862a01416581eb110966136c0f42d0bd5
Hinnerk van Bruinehsen <h.v.bruinehsen@fu-berlin.de>
2017-08-09 15:12:25 +0200

    seL4/am335x: begin of driver implementation for dmtimer 1ms

---
+++ repos/os/src/drivers/timer/dmtimer/target.mk
@@ -0,0 +1,8 @@
+TARGET   = am335x_timer_drv
+REQUIRES = wega_am335x
+INC_DIR += $(REP_DIR)/src/drivers/timer/dmtimer
+SRC_CC  += time_source.cc
+
+include $(REP_DIR)/src/drivers/timer/target.inc
+
+vpath time_source.cc $(REP_DIR)/src/drivers/timer/dmtimer
