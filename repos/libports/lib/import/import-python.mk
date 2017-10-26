1e329e72d59b6d0b60e04c3ee37d62ecab09ab62
Hinnerk van Bruinehsen <h.v.bruinehsen@fu-berlin.de>
2017-10-26 23:27:18 +0200

    Python: enable build for ARM

--- repos/libports/lib/import/import-python.mk
+++ repos/libports/lib/import/import-python.mk
@@ -6,9 +6,13 @@ ifeq ($(filter-out $(SPECS),x86),)
    ifeq ($(filter-out $(SPECS),32bit),)
      REP_INC_DIR += include/python/spec/x86_32
    endif # 32bit
- 
+
    ifeq ($(filter-out $(SPECS),64bit),)
      REP_INC_DIR += include/python/spec/x86_64
    endif # 64bit
 endif # x86
+
+ifeq ($(filter-out $(SPECS),arm),)
+	REP_INC_DIR += include/python/spec/arm
+endif #arm
 
