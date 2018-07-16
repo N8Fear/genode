b3fadb84d2f53a89dbf7a5e9494f4f931517203b
Hinnerk van Bruinehsen <h.v.bruinehsen@fu-berlin.de>
2018-07-16 23:36:17 +0200

    seL4/am335x: split board and cpu specs

---
+++ repos/base/mk/spec/am335x.mk
@@ -0,0 +1,13 @@
+#
+# \brief Build-system for the TI AM335x series CPUs
+# \author Hinnerk van Bruienhsen <hvbruinehsen@stackptr.de>
+# \date 2018-07-13
+#
+
+SPECS += arm_v7a gpio
+
+# Add repository relative include paths
+REP_INC_DIR += include/spec/am335x
+
+# include implied specs
+include $(BASE_DIR)/mk/spec/arm_v7a.mk
