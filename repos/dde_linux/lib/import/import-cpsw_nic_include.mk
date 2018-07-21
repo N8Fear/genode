63b9d0ad9961a0ab5aa2b82bae930732c01053fb
Hinnerk van Bruinehsen <h.v.bruinehsen@fu-berlin.de>
2018-07-21 23:15:11 +0200

    seL4/am335x: CPSW/NIC WIP

---
+++ repos/dde_linux/lib/import/import-cpsw_nic_include.mk
@@ -0,0 +1,5 @@
+LX_CONTRIB_DIR := $(call select_from_ports,dde_linux)/src/drivers/nic/cpsw
+SRC_DIR        := $(REP_DIR)/src/drivers/nic/cpsw
+INC_DIR        += $(LX_CONTRIB_DIR)/drivers/net/ethernet/ti
+INC_DIR        += $(LIB_CACHE_DIR)/cpsw_nic_include/include/include/include
+CC_OPT         += -U__linux__ -D__KERNEL__
